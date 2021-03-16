8a9356c015918e1deaef926499865e6f7c9eecd6
skshu_miaohaojie <q58369921>
2021-03-16 18:23:31 +0800

    代码提交

--- src/main/java/com/mhj/demo/DemoApplication.java
+++ src/main/java/com/mhj/demo/DemoApplication.java
@@ -4,11 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ServletComponentScan
 @EnableSwagger2
+@EnableDiscoveryClient
 public class DemoApplication {
 
     public static void main(String[] args) {
